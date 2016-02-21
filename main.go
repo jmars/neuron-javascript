cbd7c72a4954ce4313de1065ae14dc37b6fd6501
Jaye Marshall <marshall.jaye@gmail.com>
2016-02-21 03:56:01 +0000

    http events working

--- main.go
+++ main.go
@@ -7,28 +7,53 @@ import (
  "net/http"
  "path/filepath"
  "github.com/jmars/go-duktape"
+ "unsafe"
 )
 
 type DuktapeHandler struct {
-  name string
-  httpCtx []byte
-  wsCtx []byte
+  httpCode []byte
+  wsCode []byte
+  uid int
+}
+
+func copy_duk_buffer(__cbuf__ unsafe.Pointer, outSize int) []byte {
+  bufc := (*[1<<30]byte)(__cbuf__)
+  buf := make([]byte, outSize)
+  for i := 0; i < outSize; i++ {
+    buf[i] = bufc[i]
+  }
+  return buf
 }
 
 func make_context(c []byte) []byte {
   ctx := duktape.New()
-  ctx.EvalString(string(c))
+  ctx.EvalString("(" + string(c) + ")")
   ctx.DumpFunction()
   outsize := 0
-  bc := ctx.GetBuffer(-1, &outsize)
+  bc := copy_duk_buffer(ctx.GetBuffer(-1, &outsize), outsize)
   ctx.DestroyHeap()
   return bc
 }
 
+func CallBuffer(c *duktape.Context, b []byte) {
+  c.PushFixedBuffer(len(b))
+  outsize := 0
+  newbuf := (*[1<<30]byte)(c.GetBuffer(-1, &outsize))
+  for i := 0; i < outsize; i++ {
+    newbuf[i] = b[i]
+  }
+  c.LoadFunction()
+  c.Call(0)
+}
+
 func (h *DuktapeHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-  name := h.name
-  // Write it back to the client.
-  fmt.Fprintf(w, "hi %s!\n", name)
+  //uid := h.uid
+  h.uid = h.uid + 1
+  c := duktape.New()
+  CallBuffer(c, h.httpCode)
+  res := c.GetLstring(-1, 0)
+  c.DestroyHeap()
+  fmt.Fprintf(w, "%s", res)
 }
 
 func main() {
@@ -52,10 +77,20 @@ func main() {
   }
 
   ctx := DuktapeHandler{
-    name: "test",
-    httpCtx: make_context(httpCode),
-    wsCtx: make_context(websocketCode),
+    httpCode: make_context(httpCode),
+    wsCode: make_context(websocketCode),
+    uid: 0,
   }
+
+  //c := duktape.New()
+  //c.PushFixedBuffer(len(ctx.httpCtx))
+  //outsize := 0
+  //newbuf := (*[1<<30]byte)(c.GetBuffer(-1, &outsize))
+  //for i := 0; i < outsize; i++ {
+  //  newbuf[i] = ctx.httpCtx[i]
+  //}
+  //c.DestroyHeap()
+
 
   // Register our handler.
   http.ListenAndServe(":8080", &ctx)
